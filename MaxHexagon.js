7443ea0994e5093283ca39a499dfedd526b08f29
aldencaron <aldenc@live.unc.edu>
2016-11-06 01:52:02 -0400

    Hexagons

--- MaxHexagon.js
+++ MaxHexagon.js
@@ -1,16 +1,24 @@
 $( document ).ready(function() {
   // hexagon
 var canvas = document.createElement("canvas");
-canvas.style.margins="200 auto";
+canvas.style = "margin: 0 auto;";
 canvas.id = "canvas";
-canvas.width="1000";
-canvas.height="1000";
+
+var numberOfSides = 6;
+var size = 60;
+
+var width = Math.floor(Math.sqrt(3)*size*5 + 1);
+canvas.width = width;
+canvas.height="515";
+
+Xinitial = width/2 - (Math.sqrt(3) * size);
+Yinitial = 95;
 var cxt= canvas.getContext('2d');
+
+
 for( var x=0; x<3; x++){
-  var numberOfSides = 6,
-      size = 20,
-      Xcenter = 200+(Math.sqrt(3)*size*x),
-      Ycenter = 100;
+  var Xcenter = Xinitial+(Math.sqrt(3)*size*x),
+      Ycenter = Yinitial;
 
   cxt.beginPath();
   cxt.moveTo (Xcenter +  size * Math.sin(0), Ycenter +  size *  Math.cos(0));
@@ -23,10 +31,8 @@ for( var x=0; x<3; x++){
   cxt.stroke();
 }
 for( var x=0; x<4; x++){
-  var numberOfSides = 6,
-      size = 20,
-      Xcenter = 200-((1/2)*Math.sqrt(3)*size)+(Math.sqrt(3)*size*x);
-      Ycenter = 100+ (3/2*size);
+  var Xcenter = Xinitial-((1/2)*Math.sqrt(3)*size)+(Math.sqrt(3)*size*x);
+      Ycenter = Yinitial+ (3/2*size);
 
   cxt.beginPath();
   cxt.moveTo (Xcenter +  size * Math.sin(0), Ycenter +  size *  Math.cos(0));
@@ -40,10 +46,8 @@ for( var x=0; x<4; x++){
 }
 
 for( var x=0; x<5; x++){
-  var numberOfSides = 6,
-      size = 20,
-      Xcenter = 200-(Math.sqrt(3)*size)+(Math.sqrt(3)*size*x);
-      Ycenter = 100+ (3*size);
+  var Xcenter = Xinitial-(Math.sqrt(3)*size)+(Math.sqrt(3)*size*x);
+      Ycenter = Yinitial+ (3*size);
 
   cxt.beginPath();
   cxt.moveTo (Xcenter +  size * Math.sin(0), Ycenter +  size *  Math.cos(0));
@@ -56,10 +60,8 @@ for( var x=0; x<5; x++){
   cxt.stroke();
 }
 for( var x=0; x<4; x++){
-  var numberOfSides = 6,
-      size = 20,
-      Xcenter = 200-((1/2)*Math.sqrt(3)*size)+(Math.sqrt(3)*size*x);
-      Ycenter = 100+ (3*(3/2)*size);
+  var Xcenter = Xinitial-((1/2)*Math.sqrt(3)*size)+(Math.sqrt(3)*size*x);
+      Ycenter = Yinitial+ (3*(3/2)*size);
 
   cxt.beginPath();
   cxt.moveTo (Xcenter +  size * Math.sin(0), Ycenter +  size *  Math.cos(0));
@@ -73,10 +75,8 @@ for( var x=0; x<4; x++){
 }
 
 for( var x=0; x<3; x++){
-  var numberOfSides = 6,
-      size = 20,
-      Xcenter = 200+(Math.sqrt(3)*size*x),
-      Ycenter = 100+ (6*size);
+  var Xcenter = Xinitial+(Math.sqrt(3)*size*x),
+      Ycenter = Yinitial+ (6*size);
 
   cxt.beginPath();
   cxt.moveTo (Xcenter +  size * Math.sin(0), Ycenter +  size *  Math.cos(0));
@@ -89,6 +89,6 @@ for( var x=0; x<3; x++){
   cxt.stroke();
 }
 
-  document.body.appendChild(canvas);
+  document.getElementById("board").append(canvas);
 
 });
