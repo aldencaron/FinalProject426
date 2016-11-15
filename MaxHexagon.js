ae1260161196bb5f58ec181a48ef180f9f10628b
aldencaron <aldenc@live.unc.edu>
2016-11-15 01:15:14 -0500

    clicking

--- MaxHexagon.js
+++ MaxHexagon.js
@@ -68,15 +68,16 @@ $(document).ready(function() {
   }
   var board;
 
+
   // Set up random board  
   var drawBoard = function() {
 	
 	// Set up canvas
     var canvas = document.getElementById("board_canvas");
     canvas.style = "margin: 0 auto;";
-
+ 
+	var size = 60;
     var num_sides = 6;
-    var size = 60;
 	var radius = 20;
 
     var width = Math.floor(Math.sqrt(3) * size * 5 + 1);
@@ -183,7 +184,46 @@ $(document).ready(function() {
   
   var board_canvas = document.getElementById("board_canvas");
   board_canvas.addEventListener('mousedown', function(e){
-	alert("clicked the canvas yo");  
+	var rect = board_canvas.getBoundingClientRect();
+	var x = (((e.clientX - rect.left)/(rect.right - rect.left) * board_canvas.width));
+	var y = (((e.clientY - rect.top)/(rect.bottom - rect.top) * board_canvas.height));
+	
+	var checkTopLeft = function(a1, a2, b1, b2, c1, c2) {
+		var m = (b2 - a2)/(b1 - a1);
+		var b = a2 - m * a1;
+		return (c2 <= (m * c1 + b));
+	}
+	var checkTopRight = function(a1, a2, b1, b2, c1, c2) {
+		var m = (b2 - a2)/(b1 - a1);
+		var b = a2 - m * a1;
+		return (c2 <= (m * c1 + b));	}
+	var checkBottomLeft = function(a1, a2, b1, b2, c1, c2) {
+		var m = (b2 - a2)/(b1 - a1);
+		var b = a2 - m * a1;
+		return (c2 >= (m * c1 + b));	}
+	var checkBottomRight = function(a1, a2, b1, b2, c1, c2) {
+		var m = (b2 - a2)/(b1 - a1);
+		var b = a2 - m * a1;
+		return (c2 >= (m * c1 + b));	}
+	var checkLeft = function(x0, x1) {
+		return (x1 >= x0);
+	}
+	var checkRight = function(x0, x1) {
+		return (x1 <= x0);
+	}
+	
+	for(var i = 0; i < board.num_pieces; i++){
+		if(checkLeft(board.tiles[i].x_coords[4], x) 
+		&& checkRight(board.tiles[i].x_coords[1], x)
+		&& checkTopLeft(board.tiles[i].x_coords[5], board.tiles[i].y_coords[5], board.tiles[i].x_coords[0], board.tiles[i].y_coords[0], x, y)
+		&& checkTopRight(board.tiles[i].x_coords[0], board.tiles[i].y_coords[0], board.tiles[i].x_coords[1], board.tiles[i].y_coords[1], x, y)
+		&& checkBottomLeft(board.tiles[i].x_coords[2], board.tiles[i].y_coords[2], board.tiles[i].x_coords[3], board.tiles[i].y_coords[3], x, y)
+		&& checkBottomRight(board.tiles[i].x_coords[3], board.tiles[i].y_coords[3], board.tiles[i].x_coords[4], board.tiles[i].y_coords[4], x, y)) {
+			alert(board.tiles[i].type + ", " + board.tiles[i].number);
+			break;
+		}
+	}
+	
   })
 
 });
