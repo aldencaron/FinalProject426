e08f54c6d8fa8ac119d35e7c524cb6d5345c57bc
Max Booth <mbooth9577@yahoo.com>
2016-11-29 21:55:16 -0500

    finished up setup

--- SettlersSetUp.php
+++ SettlersSetUp.php
@@ -3,9 +3,9 @@
 date_default_timezone_set('America/New_York');
 
 $mysqli = new mysqli("classroom.cs.unc.edu",
-                   "naeimz",
-                   "alphabetazetaomega",
-		               "naeimzdb");
+                   "mhb",
+                   "password",
+		               "mhbdb");
 $SQL = "drop table if exists Players";
 mysqli_query($mysqli, $SQL);
 $SQL = "drop table if exists Roads";
@@ -25,8 +25,8 @@ $SQL = "Create Table Players (
   Points int)";
 mysqli_query($mysqli, $SQL);
 //4 players
-for($i = 0; $i < 4; $i++){
-  $SQL = "Insert into Players (PlayerID, Username, Roadscount. SoldiersCount, Points)
+for($i = 0; $i <= 4; $i++){
+  $SQL = "Insert into Players (PlayerID, Username, Roadscount, SoldiersCount, Points)
   Values ($i, 0, 0, 0, 0)";
     mysqli_query($mysqli, $SQL);
 }
@@ -37,9 +37,9 @@ $SQL = "Create Table Roads (
   Available int)";
 mysqli_query($mysqli, $SQL);
 //73 roads
-for($i = 0; $i < 4; $i++){
-  $SQL = "Insert into Players (PlayerID, Username, Roadscount. SoldiersCount, Points)
-  Values ($i, 0, 0, 0, 0)";
+for($i = 0; $i <= 73; $i++){
+  $SQL = "Insert into Roads (RoadID, PlayerID, Available)
+  Values ($i, 0, 1)";
     mysqli_query($mysqli, $SQL);
 }
 
@@ -48,6 +48,11 @@ $SQL = "Create Table Tiles (
   Robber int)";
 mysqli_query($mysqli, $SQL);
 //19 Tiles
+for($i = 0; $i <= 19; $i++){
+  $SQL = "Insert into Tiles (ID, Robber)
+  Values ($i, 0)";
+    mysqli_query($mysqli, $SQL);
+}
 
 $SQL = "Create Table Colleges (
   CollegeID int primary key not null auto_increment,
@@ -56,7 +61,11 @@ $SQL = "Create Table Colleges (
   University int)";
 mysqli_query($mysqli, $SQL);
 //54 colleges
-
+for($i = 0; $i <= 54; $i++){
+  $SQL = "Insert into Colleges (CollegeID, PlayerID, Available, University)
+  Values ($i, 0, 0, 0)";
+    mysqli_query($mysqli, $SQL);
+}
 
   $SQL = "Create Table Cards (
     PlayerID int primary key not null auto_increment,
@@ -65,5 +74,12 @@ mysqli_query($mysqli, $SQL);
     DavisLibrary int, Sitterson int, BellTower int,
     Roads int, Volunteer int, Monopoly int)";
     $result= mysqli_query($mysqli, $SQL);
+    for($i = 0; $i <= 4; $i++){
+      $SQL = "Insert into Cards (PlayerID, Ram, Ramen, Brick,
+      Basketball, Book, Knight, OldWell, ThePit, DavisLibrary,
+      Sitterson, BellTower, Roads, Volunteer, Monopoly)
+      Values ($i, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0)";
+        mysqli_query($mysqli, $SQL);
+    }
 
  ?>
