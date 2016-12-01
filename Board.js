9579d5c4baa890c0c2381c676dec0308486c5615
aldencaron <aldenc@live.unc.edu>
2016-11-30 21:33:03 -0500

    start turns vaguely

--- Board.js
+++ Board.js
@@ -6,9 +6,9 @@ $(document).ready(function() {
   var y_initial = 98;
   var num_sides = 6;
   var board;
+  var turn_over = false;
 
-  // Draw board
-  var drawAvailableBoard = function() {
+  var drawBoard = function(available_roads, available_colleges, available_universities, available_robber) {
     // Set up canvas
     var canvas = document.getElementById("board_canvas");
     canvas.style = "margin: 0 auto;";
@@ -20,30 +20,6 @@ $(document).ready(function() {
 
     var ctx = canvas.getContext('2d');
 
-    var drawTile = function(x_center, y_center) {
-      ctx.strokeStyle = 'black';
-      ctx.lineWidth = 8;
-      ctx.stroke();
-      ctx.fillStyle = ctx.createPattern(game.tiles[i].image, "repeat");
-      ctx.fill();
-
-      if (game.tiles[i].number != 0) {
-        ctx.beginPath();
-        ctx.arc(x_center, y_center, radius, 0, 2 * Math.PI, false);
-        ctx.fillStyle = 'white';
-        ctx.fill();
-        ctx.lineWidth = 1;
-        ctx.strokeStyle = 'black';
-        ctx.stroke();
-
-        ctx.fillStyle = 'black';
-        ctx.textBaseline = 'middle';
-        ctx.textAlign = "center";
-        ctx.font = "bold 26px Arial";
-        ctx.fillText(game.tiles[i].number, game.tiles[i].x_center, game.tiles[i].y_center);
-      }
-    }
-
     // Draw on tiles
     for (var i = 0; i < game.num_pieces; i++) {
       ctx.beginPath();
@@ -51,137 +27,200 @@ $(document).ready(function() {
       for (var j = 1; j <= num_sides; j++) {
         ctx.lineTo(game.tiles[i].x_coords[j], game.tiles[i].y_coords[j]);
       }
-      drawTile(game.tiles[i].x_center, game.tiles[i].y_center, i);
-
-    }
-    // Draw on possible colleges, add check for available, add check for whether to draw or not TODO
-    for (var i = 0; i < game.num_colleges; i++) {
-      if (game.colleges[i].available || game.colleges[i].used) {
+      ctx.strokeStyle = 'black';
+      ctx.lineWidth = 8;
+      ctx.stroke();
+      ctx.fillStyle = ctx.createPattern(game.tiles[i].image, "repeat");
+      ctx.fill();
+      // Draw on the number circle
+      if (game.tiles[i].number != 0 || available_robber) {
         ctx.beginPath();
-        ctx.arc(game.colleges[i].x, game.colleges[i].y, game.colleges[i].radius, 0, 2 * Math.PI, false);
-        ctx.fillStyle = 'gray';
-        if(game.colleges[i].used){ctx.fillStyle = game.colleges[i].player.color;}
+        ctx.arc(game.tiles[i].x_center, game.tiles[i].y_center, radius, 0, 2 * Math.PI, false);
+        ctx.fillStyle = 'white';
+        if(game.tiles[i].robber){ctx.fillStyle = '#001A57';}
+        if(available_robber && !game.tiles[i].robber){ctx.fillStyle = 'grey';}
         ctx.fill();
-        ctx.lineWidth = 2;
+        ctx.lineWidth = 1;
         ctx.strokeStyle = 'black';
         ctx.stroke();
+
+        // Draw on the number
+        if(!game.tiles[i].robber){
+          ctx.fillStyle = 'black';
+          ctx.textBaseline = 'middle';
+          ctx.textAlign = "center";
+          ctx.font = "bold 26px Arial";
+          ctx.fillText(game.tiles[i].number, game.tiles[i].x_center, game.tiles[i].y_center);
+        }
       }
     }
-
-    // Draw on possible roads, add check for available TODO
+    // Draw on roads
     for (var i = 0; i < game.num_roads; i++) {
-      if (game.roads[i].used || game.roads[i].available) {
+      if (game.roads[i].used) {
+        ctx.beginPath();
+        ctx.moveTo(game.roads[i].start_x, game.roads[i].start_y);
+        ctx.lineTo(game.roads[i].end_x, game.roads[i].end_y);
+        if(game.roads[i].used){ctx.fillStyle = game.roads[i].player.color;}
+        ctx.lineWidth = 8;
+        ctx.strokeStyle = game.roads[i].player.color;
+        ctx.stroke();
+      }
+      else if (available_roads && game.roads[i].available){
         ctx.beginPath();
         ctx.arc((game.roads[i].start_x + game.roads[i].end_x) / 2, (game.roads[i].start_y + game.roads[i].end_y) / 2,
         game.roads[i].radius, 0, 2 * Math.PI, false);
         ctx.fillStyle = 'gray';
-        if(game.roads[i].used){ctx.fillStyle = game.roads[i].player.color;}
         ctx.fill();
         ctx.lineWidth = 2;
         ctx.strokeStyle = 'black';
         ctx.stroke();
       }
     }
-
+    // Draw on colleges
+    for (var i = 0; i < game.num_colleges; i++) {
+      if (game.colleges[i].used) {
+        if(game.colleges[i].settlement){
+          game.colleges[i].radius = 12;
+        }
+        ctx.beginPath();
+        ctx.arc(game.colleges[i].x, game.colleges[i].y, game.colleges[i].radius, 0, 2 * Math.PI, false);
+        ctx.fillStyle = game.colleges[i].player.color;
+        ctx.fill();
+        ctx.lineWidth = 2;
+        ctx.strokeStyle = 'black';
+        ctx.stroke();
+      }
+      if(available_colleges && game.colleges[i].available){
+        ctx.beginPath();
+        ctx.arc(game.colleges[i].x, game.colleges[i].y, game.colleges[i].radius, 0, 2 * Math.PI, false);
+        ctx.fillStyle = 'gray';
+        ctx.fill();
+        ctx.lineWidth = 2;
+        ctx.strokeStyle = 'black';
+        ctx.stroke();
+      }
+      if(available_universities && !game.colleges[i].university && game.colleges[i].player == game.player){
+        ctx.beginPath();
+        ctx.arc(game.colleges[i].x, game.colleges[i].y, 12, 0, 2 * Math.PI, false);
+        ctx.fillStyle = 'gray';
+        ctx.fill();
+        ctx.lineWidth = 2;
+        ctx.strokeStyle = 'black';
+        ctx.stroke;
+      }
+    }
     document.getElementById("board").append(canvas);
-
-
   };
 
   /*var board_canvas = document.getElementById("board_canvas");
   board_canvas.addEventListener('mousedown', function(e) {
-    var rect = board_canvas.getBoundingClientRect();
-    var x = (((e.clientX - rect.left) / (rect.right - rect.left) * board_canvas.width));
-    var y = (((e.clientY - rect.top) / (rect.bottom - rect.top) * board_canvas.height));
+  var rect = board_canvas.getBoundingClientRect();
+  var x = (((e.clientX - rect.left) / (rect.right - rect.left) * board_canvas.width));
+  var y = (((e.clientY - rect.top) / (rect.bottom - rect.top) * board_canvas.height));
 
 
-    // Check hexagons
-    for (var i = 0; i < game.num_pieces; i++) {
-      if (game.checkLeft(game.tiles[i].x_coords[4], x)
-      && game.checkRight(game.tiles[i].x_coords[1], x)
-      && game.checkTopLeft(game.tiles[i].x_coords[5], game.tiles[i].y_coords[5], game.tiles[i].x_coords[0], game.tiles[i].y_coords[0], x, y)
-      && game.checkTopRight(game.tiles[i].x_coords[0], game.tiles[i].y_coords[0], game.tiles[i].x_coords[1], game.tiles[i].y_coords[1], x, y)
-      && game.checkBottomLeft(game.tiles[i].x_coords[2], game.tiles[i].y_coords[2], game.tiles[i].x_coords[3], game.tiles[i].y_coords[3], x, y)
-      && game.checkBottomRight(game.tiles[i].x_coords[3], game.tiles[i].y_coords[3], game.tiles[i].x_coords[4], game.tiles[i].y_coords[4], x, y)) {
-        alert(game.tiles[i].type + ", " + game.tiles[i].number);
-        break;
-      }
-    }
+  // Check hexagons
+  for (var i = 0; i < game.num_pieces; i++) {
+  if (game.checkLeft(game.tiles[i].x_coords[4], x)
+  && game.checkRight(game.tiles[i].x_coords[1], x)
+  && game.checkTopLeft(game.tiles[i].x_coords[5], game.tiles[i].y_coords[5], game.tiles[i].x_coords[0], game.tiles[i].y_coords[0], x, y)
+  && game.checkTopRight(game.tiles[i].x_coords[0], game.tiles[i].y_coords[0], game.tiles[i].x_coords[1], game.tiles[i].y_coords[1], x, y)
+  && game.checkBottomLeft(game.tiles[i].x_coords[2], game.tiles[i].y_coords[2], game.tiles[i].x_coords[3], game.tiles[i].y_coords[3], x, y)
+  && game.checkBottomRight(game.tiles[i].x_coords[3], game.tiles[i].y_coords[3], game.tiles[i].x_coords[4], game.tiles[i].y_coords[4], x, y)) {
+  alert(game.tiles[i].type + ", " + game.tiles[i].number);
+  break;
+}
+}
 
-  })*/
+})*/
 
-  var addCollege = function(event) {
-    var found = false;
-    var rect = board_canvas.getBoundingClientRect();
-    var x = (((event.clientX - rect.left) / (rect.right - rect.left) * board_canvas.width));
-    var y = (((event.clientY - rect.top) / (rect.bottom - rect.top) * board_canvas.height));
+var addCollegeStart = function(event) {
+  turn_over = false;
+  var rect = board_canvas.getBoundingClientRect();
+  var x = (((event.clientX - rect.left) / (rect.right - rect.left) * board_canvas.width));
+  var y = (((event.clientY - rect.top) / (rect.bottom - rect.top) * board_canvas.height));
 
-    // Check colleges
-    for (var i = 0; i < game.num_colleges; i++) {
-      // TODO add in an if statement to check if on the game -- available here only works for first
-      if(game.colleges[i].available){
-        if (game.pointDistance(x, y, game.colleges[i].x, game.colleges[i].y) <= game.colleges[i].radius) {
-          found = true;
-          game.colleges[i].used = true;
-          game.colleges[i].available = false;
-          game.player.colleges.push(game.colleges[i]);
-          game.colleges[i].player = game.player;
-          for(var j = 0; j < game.num_roads; j++){
-            if(game.roads[j].connections[0].id == game.colleges[i].id){
-              game.roads[j].connections[1].available = false;
-              if(!game.roads[j].used){
-                game.roads[j].available = true;
-              }
+  // Check colleges
+  for (var i = 0; i < game.num_colleges; i++) {
+    // TODO add in an if statement to check if on the game -- available here only works for first
+    if(game.colleges[i].available){
+      if (game.pointDistance(x, y, game.colleges[i].x, game.colleges[i].y) <= game.colleges[i].radius) {
+        turn_over = true;
+        game.colleges[i].used = true;
+        game.colleges[i].available = false;
+        game.player.colleges.push(game.colleges[i]);
+        game.colleges[i].player = game.player;
+        for(var j = 0; j < game.num_roads; j++){
+          game.roads[j].available = false;
+          if(game.roads[j].connections[0].id == game.colleges[i].id){
+            game.roads[j].connections[1].available = false;
+            if(!game.roads[j].used){
+              game.roads[j].available = true;
             }
-            else if(game.roads[j].connections[1].id == game.colleges[i].id){
-              game.roads[j].connections[0].available = false;
-              if(!game.roads[j].used){
-                game.roads[j].available = true;
-              }
-            }
-            game.colleges[i].radius = 10;
-            drawAvailableBoard();
           }
+          else if(game.roads[j].connections[1].id == game.colleges[i].id){
+            game.roads[j].connections[0].available = false;
+            if(!game.roads[j].used){
+              game.roads[j].available = true;
+            }
+          }
+          game.colleges[i].radius = 10;
+          drawBoard(true, false, false, false);
         }
       }
     }
-    if(found){
-      board_canvas.removeEventListener('mousedown', addCollege);
-    }
-  };
-  var addRoad = function() {
-    var found = false;
-    var rect = board_canvas.getBoundingClientRect();
-    var x = (((event.clientX - rect.left) / (rect.right - rect.left) * board_canvas.width));
-    var y = (((event.clientY - rect.top) / (rect.bottom - rect.top) * board_canvas.height));
+  }
+  if(turn_over){
+    board_canvas.removeEventListener('mousedown', addCollegeStart);
+    turn_over = false;
+  }
+};
+var addRoadStart = function() {
+  turn_over = false;
+  var rect = board_canvas.getBoundingClientRect();
+  var x = (((event.clientX - rect.left) / (rect.right - rect.left) * board_canvas.width));
+  var y = (((event.clientY - rect.top) / (rect.bottom - rect.top) * board_canvas.height));
 
-    // Check roads
-    for (var i = 0; i < game.num_roads; i++) {
-      if(game.roads[i].available){
-        if (game.pointDistance(x, y, game.roads[i].x_center, game.roads[i].y_center) <= game.roads[i].radius) {
-          found = true;
-          game.roads[i].used = true;
-          game.roads[i].available = false;
-          game.player.roads.push(game.roads[i]);
-          game.roads[i].player = game.player;
-          //TODO add new available colleges as we come
-          game.roads[i].radius = 12;
-          drawAvailableBoard();
+  // Check roads
+  for (var i = 0; i < game.num_roads; i++) {
+    if(game.roads[i].available){
+      if (game.pointDistance(x, y, game.roads[i].x_center, game.roads[i].y_center) <= game.roads[i].radius) {
+        turn_over = true;
+        game.roads[i].used = true;
+        game.roads[i].available = false;
+        game.player.roads.push(game.roads[i]);
+        game.roads[i].player = game.player;
+        //TODO add new available colleges as we come
+        game.roads[i].radius = 12;
+        drawBoard(false, false, false, false);
+        if(game.turn_number > 2){
+          //ADD IN NEW AVAILABLE COLLEGES AND STUFF
+
         }
       }
     }
-    if(found){
-      board_canvas.removeEventListener('mousedown', addRoad);
-    }
-  };
-  var firstTurn = function() {
-    var board_canvas = document.getElementById("board_canvas");
-    board_canvas.addEventListener('mousedown', addCollege);
-    board_canvas.addEventListener('mousedown', addRoad);
-  };
+  }
+  if(turn_over){
+    board_canvas.removeEventListener('mousedown', addRoadStart);
+    game.turn_number++;
+    //TODO right here check if other turns have passed or not idk what
+    // while(game.turn_number != 2){hang until is turn again then proceed}
+    if(game.turn_number == 2){firstTurn();}
+  }
+};
+var firstTurn = function() {
+  turn_over = false;
+  drawBoard(false, true, false, false);
+  var board_canvas = document.getElementById("board_canvas");
+  board_canvas.addEventListener('mousedown', addCollegeStart);
+  board_canvas.addEventListener('mousedown', addRoadStart);
+};
 
-  var game = new SettlersGame();
-  game.startGame();
-  drawAvailableBoard();
+var game = new SettlersGame();
+game.startGame();
+drawBoard(false, false, false, false);
+if(game.turn_number == 1){
+  alert("First Turn");
   firstTurn();
+}
 });
--- Game.js
+++ Game.js
@@ -1,5 +1,24 @@
+var SETTLERS_CONSTANTS =
+{
+  // Game sections
+  REGISTERING_PLAYERS: 0,
+  SETTING_UP_BOARD: 1,
+  HOME_PLAYER: 2,
+  OTHER_PLAYER_ONE: 3,
+  OTHER_PLAYER_TWO: 4,
+  OTHER_PLAYER_THREE: 5,
+  FINISHED: 6,
+
+  // Events
+  TURN_CHANGE_EVENT: 0,
+  TRADE_EVENT: 1
+};
+
+
 var SettlersGame = function() {
-  var imageNames = ["field.jpg", "paper.jpg", "mountain.jpg", "brickwall.jpg", "pasta.jpg", "desert.jpg"];
+
+  var imageNames = ["images/field.jpg", "images/paper.jpg",  "images/mountain.jpg",
+  "images/brickwall.jpg", "images/pasta.jpg",  "images/desert.jpg"];
   var images = [];
   for (var i = 0; i < imageNames.length; i++) {
     var image = new Image();
@@ -7,10 +26,7 @@ var SettlersGame = function() {
     images.push(image);
   }
 
-  // TODO FIX
-  alert("The images are not going to load rn so we should fix this.");
-
-  // Globals TODO figure out which ones you need here versus in the other
+  // Globals
   this.size = 60;
   this.width = Math.floor(Math.sqrt(3) * this.size * 5 + 1);
   this.x_initial = this.width / 2 - (Math.sqrt(3) * this.size) + 8;
@@ -21,6 +37,7 @@ var SettlersGame = function() {
   this.num_colleges = 54;
   this.num_roads = 73;
 
+
   // Game objects
   this.tiles = [];
   this.colleges = [];
@@ -28,7 +45,10 @@ var SettlersGame = function() {
   this.board;
   this.player;
   this.other_players = [];
+  this.registered_event_handlers = {};
 
+  this.turn_number = 0;
+  this.status;
 
   // Roll dice
   this.diceRoll = function(num_dice) {
@@ -78,33 +98,32 @@ var SettlersGame = function() {
     this.x_coords = [];
     this.y_coords = [];
     this.colleges = [];
-    // Add in location when it is assigned
-    // Add in colleges as it goes
+    this.robber = false;
   }
   //TODO add player ids when gotten
-  var gamePlayer = function(){
-      this.key = Math.floor(Math.random() * 100000);
-      this.color = "green";
-      this.num_cards = 0;
-      this.cards = [];
-      this.cards["ramen"] = [];
-      this.cards["book"] = [];
-      this.cards["ram"] = [];
-      this.cards["brick"] = [];
-      this.cards["basketball"] = [];
-      this.cards["knight"] = [];
-      this.cards["victory_points"] = [];
-      this.cards["monopoly"] = [];
-      this.cards["volunteer"] = [];
-      this.cards["roads"] = [];
-      this.colleges = [];
-      this.roads = [];
+  var gamePlayer = function() {
+    this.key = Math.floor(Math.random() * 100000);
+    this.color = "green";
+    this.num_cards = 0;
+    this.cards = [];
+    this.cards["ramen"] = [];
+    this.cards["book"] = [];
+    this.cards["ram"] = [];
+    this.cards["brick"] = [];
+    this.cards["basketball"] = [];
+    this.cards["knight"] = [];
+    this.cards["victory_points"] = [];
+    this.cards["monopoly"] = [];
+    this.cards["volunteer"] = [];
+    this.cards["roads"] = [];
+    this.colleges = [];
+    this.roads = [];
   }
   var gameOtherPlayer = function(color){
-      this.num_cards = 0;
-      this.colleges = [];
-      this.roads = [];
-      this.color = color;
+    this.num_cards = 0;
+    this.colleges = [];
+    this.roads = [];
+    this.color = color;
   }
 
   // Make one board object per game
@@ -124,17 +143,15 @@ var SettlersGame = function() {
 
     // Need certain amounts of each pattern type
     this.pieces = [images[0], images[0], images[0], images[0],
-      images[1], images[1], images[1], images[1],
-      images[2], images[2], images[2],
-      images[3], images[3], images[3],
-      images[4], images[4], images[4], images[4],
-      images[5]
-    ];
+    images[1], images[1], images[1], images[1],
+    images[2], images[2], images[2],
+    images[3], images[3], images[3],
+    images[4], images[4], images[4], images[4],
+    images[5]];
 
     // Set up tile types
     this.types = ["FIELD", "FIELD", "FIELD", "FIELD", "PAPER", "PAPER", "PAPER", "PAPER",
-      "MOUNTAIN", "MOUNTAIN", "MOUNTAIN", "BRICK", "BRICK", "BRICK", "PASTA", "PASTA", "PASTA", "PASTA", "DESERT"
-    ];
+    "MOUNTAIN", "MOUNTAIN", "MOUNTAIN", "BRICK", "BRICK", "BRICK", "PASTA", "PASTA", "PASTA", "PASTA", "DESERT"];
 
     // Set up tile coordinates
     var x_center, y_center;
@@ -173,7 +190,10 @@ var SettlersGame = function() {
       } else {
         tile_number = this.numbers_placement[i];
       }
-      var new_tile = new gameTile(tile_image, tile_type, tile_number, i)
+      var new_tile = new gameTile(tile_image, tile_type, tile_number, i);
+      if(new_tile.type == "DESERT") {
+        new_tile.robber = true;
+      }
       this.tiles.push(new_tile);
       new_tile.x_center = x_centers[i];
       new_tile.y_center = y_centers[i];
@@ -184,9 +204,7 @@ var SettlersGame = function() {
         new_tile.y_coords[j] = new_tile.y_center + this.size * Math.cos(j * 2 * Math.PI / this.num_sides);
       }
     }
-    //this.tiles = tiles;
 
-    //TODO make a loop if you feel like it
     var colleges_tiles = [0, 1, 2, 0, 1, 2, 2, 0, 1, 2, 2, 3, 4, 5, 6, 6, 3, 4, 5, 6, 6, 7, 8, 9, 10, 11, 11,
       7, 8, 9, 10, 11, 11, 12, 13, 14, 15, 15, 12, 13, 14, 15, 15, 16, 17, 18, 18, 16, 17, 18, 18, 16, 17, 18
     ];
@@ -258,128 +276,137 @@ var SettlersGame = function() {
       }
       colleges[i] = new gameCollege(this.tiles[colleges_tiles[i]].x_coords[colleges_coords[i]],
         this.tiles[colleges_tiles[i]].y_coords[colleges_coords[i]], adj_tiles_loop, i);
+      }
+
+      this.colleges = colleges;
+
+      var roads = [];
+      var connections = [
+        [3, 0],
+        [0, 4],
+        [4, 1],
+        [1, 5],
+        [5, 2],
+        [2, 6],
+        [3, 7],
+        [4, 8],
+        [5, 9],
+        [6, 10],
+        [11, 7],
+        [7, 12],
+        [12, 8],
+        [8, 13],
+        [13, 9],
+        [9, 14],
+        [14, 10],
+        [10, 15],
+        [11, 16],
+        [12, 17],
+        [13, 18],
+        [14, 19],
+        [15, 20],
+        [21, 16],
+        [16, 22],
+        [22, 17],
+        [17, 23],
+        [23, 18],
+        [18, 24],
+        [24, 19],
+        [19, 25],
+        [25, 20],
+        [20, 26],
+        [21, 27],
+        [22, 28],
+        [23, 29],
+        [24, 30],
+        [25, 31],
+        [26, 32],
+        [27, 33],
+        [33, 28],
+        [28, 34],
+        [34, 29],
+        [29, 35],
+        [35, 30],
+        [30, 36],
+        [36, 31],
+        [31, 37],
+        [37, 32],
+        [33, 38],
+        [34, 39],
+        [35, 40],
+        [36, 41],
+        [37, 42],
+        [38, 43],
+        [43, 39],
+        [39, 44],
+        [44, 40],
+        [40, 45],
+        [45, 41],
+        [41, 46],
+        [46, 42],
+        [43, 47],
+        [44, 48],
+        [45, 49],
+        [46, 50],
+        [47, 51],
+        [51, 48],
+        [48, 52],
+        [52, 49],
+        [49, 53],
+        [53, 50],
+        [50, 53]
+      ]
+      for (var i = 0; i < connections.length; i++) {
+        roads.push(new gameRoad(colleges[connections[i][0]], colleges[connections[i][1]], i));
+      }
+      this.roads = roads;
+      this.player = new gamePlayer();
+      this.other_players.push(new gameOtherPlayer('red'));
+      this.other_players.push(new gameOtherPlayer('blue'));
+      this.other_players.push(new gameOtherPlayer('yellow'));
     }
 
-    this.colleges = colleges;
-
-    var roads = [];
-    var connections = [
-      [3, 0],
-      [0, 4],
-      [4, 1],
-      [1, 5],
-      [5, 2],
-      [2, 6],
-      [3, 7],
-      [4, 8],
-      [5, 9],
-      [6, 10],
-      [11, 7],
-      [7, 12],
-      [12, 8],
-      [8, 13],
-      [13, 9],
-      [9, 14],
-      [14, 10],
-      [10, 15],
-      [11, 16],
-      [12, 17],
-      [13, 18],
-      [14, 19],
-      [15, 20],
-      [21, 16],
-      [16, 22],
-      [22, 17],
-      [17, 23],
-      [23, 18],
-      [18, 24],
-      [24, 19],
-      [19, 25],
-      [25, 20],
-      [20, 26],
-      [21, 27],
-      [22, 28],
-      [23, 29],
-      [24, 30],
-      [25, 31],
-      [26, 32],
-      [27, 33],
-      [33, 28],
-      [28, 34],
-      [34, 29],
-      [29, 35],
-      [35, 30],
-      [30, 36],
-      [36, 31],
-      [31, 37],
-      [37, 32],
-      [33, 38],
-      [34, 39],
-      [35, 40],
-      [36, 41],
-      [37, 42],
-      [38, 43],
-      [43, 39],
-      [39, 44],
-      [44, 40],
-      [40, 45],
-      [45, 41],
-      [41, 46],
-      [46, 42],
-      [43, 47],
-      [44, 48],
-      [45, 49],
-      [46, 50],
-      [47, 51],
-      [51, 48],
-      [48, 52],
-      [52, 49],
-      [49, 53],
-      [53, 50],
-      [50, 53]
-    ]
-    for (var i = 0; i < connections.length; i++) {
-      roads.push(new gameRoad(colleges[connections[i][0]], colleges[connections[i][1]], i));
+    // Helper methods
+    this.pointDistance = function(a1, a2, b1, b2) {
+      return (Math.sqrt((Math.pow(a1 - b1, 2)) + Math.pow(a2 - b2, 2)));
     }
-    this.roads = roads;
-    this.player = new gamePlayer();
-    this.other_players.push(new gameOtherPlayer('red'));
-    this.other_players.push(new gameOtherPlayer('blue'));
-    this.other_players.push(new gameOtherPlayer('yellow'));
-  }
+    this.checkTopLeft = function(a1, a2, b1, b2, c1, c2) {
+      var m = (b2 - a2) / (b1 - a1);
+      var b = a2 - m * a1;
+      return (c2 <= (m * c1 + b));
+    }
+    this.checkTopRight = function(a1, a2, b1, b2, c1, c2) {
+      var m = (b2 - a2) / (b1 - a1);
+      var b = a2 - m * a1;
+      return (c2 <= (m * c1 + b));
+    }
+    this.checkBottomLeft = function(a1, a2, b1, b2, c1, c2) {
+      var m = (b2 - a2) / (b1 - a1);
+      var b = a2 - m * a1;
+      return (c2 >= (m * c1 + b));
+    }
+    this.checkBottomRight = function(a1, a2, b1, b2, c1, c2) {
+      var m = (b2 - a2) / (b1 - a1);
+      var b = a2 - m * a1;
+      return (c2 >= (m * c1 + b));
+    }
+    this.checkLeft = function(x0, x1) {
+      return (x1 >= x0);
+    }
+    this.checkRight = function(x0, x1) {
+      return (x1 <= x0);
+    }
 
-  // Helper methods
-  this.pointDistance = function(a1, a2, b1, b2) {
-    return (Math.sqrt((Math.pow(a1 - b1, 2)) + Math.pow(a2 - b2, 2)));
-  }
-  this.checkTopLeft = function(a1, a2, b1, b2, c1, c2) {
-    var m = (b2 - a2) / (b1 - a1);
-    var b = a2 - m * a1;
-    return (c2 <= (m * c1 + b));
-  }
-  this.checkTopRight = function(a1, a2, b1, b2, c1, c2) {
-    var m = (b2 - a2) / (b1 - a1);
-    var b = a2 - m * a1;
-    return (c2 <= (m * c1 + b));
-  }
-  this.checkBottomLeft = function(a1, a2, b1, b2, c1, c2) {
-    var m = (b2 - a2) / (b1 - a1);
-    var b = a2 - m * a1;
-    return (c2 >= (m * c1 + b));
-  }
-  this.checkBottomRight = function(a1, a2, b1, b2, c1, c2) {
-    var m = (b2 - a2) / (b1 - a1);
-    var b = a2 - m * a1;
-    return (c2 >= (m * c1 + b));
-  }
-  this.checkLeft = function(x0, x1) {
-    return (x1 >= x0);
-  }
-  this.checkRight = function(x0, x1) {
-    return (x1 <= x0);
-  }
+    this.registerEventHandler = function(event_type, handler) {
+      if (registeredEventHandlers[event_type] == null) {
+        registeredEventHandlers[event_type] = new Array();
+      }
+      registeredEventHandlers[event_type].push(handler);
+    }
 
-  this.startGame = function() {
-    board = this.gameBoard();
+    this.startGame = function() {
+      board = this.gameBoard();
+      this.turn_number = 1;
+      this.status = SETTLERS_CONSTANTS.REGISTERING_PLAYERS;
+    }
   }
-}
