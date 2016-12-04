e36f83e4a660e912dcc7341565e7d5e5484d74fb
aldencaron <aldenc@live.unc.edu>
2016-12-04 15:03:28 -0500

    shelly's stuff

--- server.js
+++ server.js
@@ -4,22 +4,28 @@ var app = express();
 var server = http.createServer(app);
 var io = require('socket.io').listen(server);
 var path = require('path');
+var usernames = [];
 
 
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/TarHeelSettlers_Mockup.html');
   app.use(express.static(path.join(__dirname, 'public')));
+  
 });
 
-
 io.on('connection', function(socket){
+	console.log('a user was added');
   socket.on('chat message', function(msg){
   	console.log('message: ' + msg);
     io.emit('chat message', msg);
+
   });
+   socket.on('turn event', function (msg) {
+        console.log("Turn Over Button Pressed!");
+    });
 });
 
-server.listen(3000, function(){
+server.listen(process.env.PORT || 3000, function(){
   console.log('listening on *:3000');
 });
